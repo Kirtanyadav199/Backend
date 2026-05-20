15a7bbe5a71adcb3422de9c757879cd87d07a5a9
Kirtanyadav199 <kirtanyadav24@gmail.com>
2026-05-20 11:55:28 +0530

    Added the Create note feature in the frontend

--- Day9/frontend/src/App.jsx
+++ Day9/frontend/src/App.jsx
@@ -1,36 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
 
-  const[notes,setNotes]= useState([
-    {
-    title:"test title 1",
-    description:"test description 1"
-  },
-   {
-    title:"test title 2",
-    description:"test description 2"
-  }, {
-    title:"test title 3",
-    description:"test description 3"
-  }, {
-    title:"test title 4",
-    description:"test description 4"
-  },
+  const[notes,setNotes]= useState([])
 
-])
   
-
-   axios.get('http://localhost:3000/api/notes')
+  function fetchNotes(){
+    axios.get('http://localhost:3000/api/notes')
    .then((res)=>{
     setNotes(res.data.note)
    })
+  }
 
+
+  
+  useEffect(()=>{
+    fetchNotes()
+  },[])
+   
+
+  function handleSubmit(e){
+
+    e.preventDefault();
+    const {title,description} = e.target.elements
+
+    console.log(title.value,description.value);
+
+    axios.post("http://localhost:3000/api/notes",{
+      title:title.value,
+      description:description.value
+    }).then(res=>{
+      console.log(res.data);
+      
+    })
+    
+    
+  }
   return (
-
-
     <>
+    <form className='note-create-form' onSubmit={handleSubmit}>
+       <input name='title' type="text" placeholder='Enter title'/>
+       <input name='description' type="text"  placeholder='Enter description'/>
+       <button>Create note</button>
+    </form>
+
     <div className="notes">
 
       {notes.map((note)=>{
