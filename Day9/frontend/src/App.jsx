388437b6397320f0352b948ac6055fc0d4a9ff4c
Kirtanyadav199 <kirtanyadav24@gmail.com>
2026-05-21 11:18:28 +0530

    Added the Edit Note feature in Frontend

--- Day9/frontend/src/App.jsx
+++ Day9/frontend/src/App.jsx
@@ -1,151 +1,146 @@
+
+
+
+
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
-
-  const[notes,setNotes]= useState([])
-
-  const[showModal,setShowModal] = useState(false)
-  
-  const[updatedTitle,setUpdatedTitle] = useState("")
-  const[updatedDescription,setUpdatedDescription] = useState("")
-
-  const[selectedNote,setSelectedNote] = useState(null)
-  
  
-  
+  const[title,setTitle] = useState('')
+  const[description,setDescription] = useState('')
+  const[notes,setNotes] = useState([])
+
+  const[showEditPanel,setShowEditPanel] = useState(false)
+  const[updateTitle,setUpdateTitle] = useState('')
+  const[updatedescription,setUpdateDescription] = useState('')
+  const[noteid,setNoteid] = useState(null)
+
+
   function fetchNotes(){
-    axios.get('http://localhost:3000/api/notes')
-   .then((res)=>{
-    setNotes(res.data.note)
-   })
+    axios.get("http://localhost:3000/api/notes")
+    .then((res)=>{
+      setNotes(res.data.note)
+    })
   }
 
   useEffect(()=>{
     fetchNotes()
   },[])
-   
+
 
   function handleSubmit(e){
-
-    e.preventDefault();
-    const {title,description} = e.target.elements
-
-    console.log(title.value,description.value);
-
+    e.preventDefault()
     axios.post("http://localhost:3000/api/notes",{
-      title:title.value,
-      description:description.value
-    }).then(res=>{
-      console.log(res.data);
-      fetchNotes()
-      
-    })
-    
-    
-  }
-
-  function handleDeleteNote(noteId){
-   axios.delete("http://localhost:3000/api/notes/"+noteId)
-   .then(res=>{
-    console.log(res.data);
-    fetchNotes()
-   })
-    
-  }
-
-  function openEditModal(note){
-    setShowModal(true)
-   
-    setSelectedNote(note)
-       
-    //  setUpdatedTitle(note.title)
-    //  setUpdatedDescription(note.description)
-
-
-  }
-
-  
-  function saveChanges(){
-
-    axios.patch(`http://localhost:3000/api/notes/${selectedNote._id}`,{
-      title:updatedTitle,
-      description:updatedDescription
+      title:title,
+      description:description
     }).then((res)=>{
       console.log(res.data);
+      setTitle('')
+      setDescription('')
       fetchNotes()
-      setShowModal(false)
-      
+    })
+
+  }
+
+  function handleDelete(noteId){
+       
+    axios.delete(`http://localhost:3000/api/notes/${noteId}`)
+    .then((res)=>{
+      fetchNotes()
     })
   }
 
+  function openEditPanel(note){
+    setShowEditPanel(true)
+    setNoteid(note._id)
+
+  }
+
+  function saveChanges(){
+
+    axios.patch(`http://localhost:3000/api/notes/${noteid}`,{
+      title:updateTitle,
+      description:updatedescription
+    }).then(()=>{
+      setShowEditPanel(false)
+      setUpdateDescription('')
+      setUpdateTitle('')
+      fetchNotes()
+    })
+  }
+ 
   return (
     <>
 
-    {
-      showModal &&(
-        <div className='modal'>
-             <div className="modal-content">
-              <h2>Edit Note</h2>
-               
-               <input 
-               type="text" 
-               placeholder='Update Title'
-               value={updatedTitle}
-               onChange={(e)=>{ setUpdatedTitle(e.target.value)}}
-               />
-
-               <input 
-               type="text"
-               placeholder='Update description'
-               value={updatedDescription}
-               onChange={(e)=>{setUpdatedDescription(e.target.value)}}
-                />
-
-                <button onClick={saveChanges} >
-                  Save Changes
-               </button>
-
-               <button onClick={()=>{
-                setShowModal(false)
-               }}>Cancle</button>
-             </div>
+        {/** Edit notes */}
+    {showEditPanel && (
+       <div className="edit-panel">
+        <div className="edit-panel-content">
+          <h2>Edit Note</h2>
+          <input 
+          type="text"
+          placeholder='Update title'
+          value={updateTitle}
+          onChange={(e)=>{
+            setUpdateTitle(e.target.value)
+          }}
+           />
+           <input 
+           type="text"
+           placeholder='Update description'
+           value={updatedescription}
+           onChange={(e)=>{
+            setUpdateDescription(e.target.value)
+           }} />
+           <button 
+           onClick={()=>{
+             saveChanges()
+           }}>Save changes</button>
+           <button onClick={()=>{
+            setShowEditPanel(false)
+           }}>Clear</button>
         </div>
-      )
-    }
+       </div>
+    )}
 
 
-
-
-
-
-
-
-    <form className='note-create-form' onSubmit={handleSubmit}>
-       <input name='title' type="text" placeholder='Enter title'/>
-       <input name='description' type="text"  placeholder='Enter description'/>
-       <button>Create note</button>
+    { /* Create notes */ }
+    <form onSubmit={handleSubmit}>
+      <input 
+      type="text"
+      placeholder='Enter title'
+      value={title}
+      onChange={(e)=>{setTitle(e.target.value)}}
+       />
+       <input 
+      type="text"
+      placeholder='Enter description'
+      value={description}
+      onChange={(e)=>{setDescription(e.target.value)}}
+       />
+      <button>Create</button>
     </form>
 
-    
-
-    <div className="notes">
-
+    <div className="all-notes">
       {notes.map((note)=>{
-        return <div className="note">
-      <h1>{note.title}</h1>
-      <p>{note.description}</p>
-      <button onClick={()=>{handleDeleteNote(note._id)}}>Delete</button>
-      <button onClick={()=>{
-        openEditModal(note)
-      }}>Edit</button>
-       
-      
-    
-       
-      </div>
+        return <div className='note'>
+          <h2>{note.title}</h2>
+          <p>{note.description}</p>
+          <button
+           className='delete-btn'
+            onClick={()=>{handleDelete(note._id)}}>
+            Delete
+            </button>
+            <button 
+            className='edit-btn'
+            onClick={()=>{
+              openEditPanel(note)
+            }}>
+              Edit
+              </button>
+        </div>
       })}
-      
     </div>
     </>
   )
