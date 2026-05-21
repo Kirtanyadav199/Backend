ad2f8c80382817c777067caccf878b7f8179de1b
Kirtanyadav199 <kirtanyadav24@gmail.com>
2026-05-21 15:39:45 +0530

    Made changes in .env

--- Day9/backend/src/config/database.js
+++ Day9/backend/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 function connectToDb(){
-    mongoose.connect(process.env.MONGO_URL)
+    mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
         console.log("Connect to DB");
         
