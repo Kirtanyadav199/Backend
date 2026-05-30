8664aef175f3940c8fce117f5282f2bea4243c23
Kirtanyadav199 <kirtanyadav24@gmail.com>
2026-05-30 10:53:47 +0530

    implement get-me API in backend

--- Day13/backend/src/controllers/auth.controllers.js
+++ Day13/backend/src/controllers/auth.controllers.js
@@ -131,7 +131,23 @@ const LoginController = asyncHandler( async(req,res)=>{
     })
 })
 
+const getMeController = asyncHandler(async(req,res)=>{
+    const userId = req.user.id
+
+    const user = await userModel.findById(userId)
+
+    res.status(200).json({
+        user:{
+            username:user.username,
+            email:user.email,
+            bio:user.bio,
+            profileImage:user.profileImage
+        }
+    })
+})
+
 module.exports = {
     RegisterController,
-    LoginController
+    LoginController,
+    getMeController
 }
--- Day13/backend/src/routes/auth.routes.js
+++ Day13/backend/src/routes/auth.routes.js
@@ -4,6 +4,7 @@ const userModel = require('../models/user.model')
 const jwt  = require('jsonwebtoken')
 const authController = require('../controllers/auth.controllers')
 const authRouter = express.Router()
+const identifyUser = require('../middlewares/auth.middleware')
 
     /**  
   * POST /api/auth/register 
@@ -12,5 +13,7 @@ const authRouter = express.Router()
 authRouter.post("/register",authController.RegisterController)
 
 authRouter.post("/login",authController.LoginController)
+
+authRouter.get("/get-me",identifyUser,authController.getMeController)
 
 module.exports= authRouter
